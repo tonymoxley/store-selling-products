19c9489d8b3c7db0b365a2d7d24513ca4fd9f53d
Tony Hernandez <antoniohdezcts@icloud.com>
2023-06-03 13:23:38 -0600

    se crea la estructura principal del proyecto en spring-boot: maven

---
+++ src/main/java/org/arkham/examples/selling/products/StoreSellingProductsApplication.java
@@ -0,0 +1,13 @@
+package org.arkham.examples.selling.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSellingProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSellingProductsApplication.class, args);
+	}
+
+}
